fe29586a4d4952b94691e5affa256fa1482c8f37
eduardpauliuc <pauliucedy@gmail.com>
2023-05-03 20:24:45 +0300

    Add dppix

---
+++ dppix/custom/discriminator_model.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels, out_channels, 4, stride, 1, bias=False, padding_mode="reflect"
+            ),
+            nn.BatchNorm2d(out_channels, affine=True),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, features=None, init_type='normal', init_gain=0.02):
+        super().__init__()
+
+        if features is None:
+            features = [64, 128, 256, 512]
+
+        self.initial = nn.Sequential(
+            nn.Conv2d(
+                in_channels * 2,
+                features[0],
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                padding_mode="reflect",
+            ),
+            nn.LeakyReLU(0.2),
+        )
+
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                CNNBlock(in_channels, feature, stride=1 if feature == features[-1] else 2),
+            )
+            in_channels = feature
+
+        layers.append(
+            nn.Conv2d(
+                in_channels, 1, kernel_size=4, stride=1, padding=1, padding_mode="reflect"
+            ),
+        )
+
+        self.model = nn.Sequential(*layers)
+
+        self.initialize_weights(init_type, init_gain)
+
+    def initialize_weights(self, init_type, init_gain):
+        """Initialize network weights.
+
+        Parameters:
+            net (network)   -- network to be initialized
+            init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
+            init_gain (float)    -- scaling factor for normal, xavier and orthogonal.
+
+        We use 'normal' in the original pix2pix and CycleGAN paper. But xavier and kaiming might
+        work better for some applications. Feel free to try yourself.
+        """
+        def init_func(m):  # define the initialization function
+            classname = m.__class__.__name__
+            if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+                if init_type == 'normal':
+                    init.normal_(m.weight.data, 0.0, init_gain)
+                elif init_type == 'xavier':
+                    init.xavier_normal_(m.weight.data, gain=init_gain)
+                elif init_type == 'kaiming':
+                    init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+                elif init_type == 'orthogonal':
+                    init.orthogonal_(m.weight.data, gain=init_gain)
+                else:
+                    raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    init.constant_(m.bias.data, 0.0)
+            elif classname.find(
+                    'BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+                init.normal_(m.weight.data, 1.0, init_gain)
+                init.constant_(m.bias.data, 0.0)
+
+        self.apply(init_func)  # apply the initialization function <init_func>
+
+    def forward(self, x, y):
+        x = torch.cat([x, y], dim=1)
+        x = self.initial(x)
+        x = self.model(x)
+        return x
+
+
+def test():
+    x = torch.randn((1, 3, 256, 256))
+    y = torch.randn((1, 3, 256, 256))
+    model = Discriminator(in_channels=3)
+    preds = model(x, y)
+    print(model)
+    print(preds.shape)
+
+
+if __name__ == "__main__":
+    test()
